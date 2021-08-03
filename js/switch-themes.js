ff39c862a2f9bef89611ef96e837ccd05b8b41ca
Haseeb <haseebs@udel.edu>
2021-08-03 13:12:26 -0400

    move toggleLightMode to HTML

--- js/switch-themes.js
+++
@@ -1,4 +0,0 @@
-function toggleLightMode() {
-    const theme = document.querySelector("#theme-link");
-    theme.href = (theme.getAttribute("href") == "css/dark-mode.css") ? "css/light-mode.css" : "css/dark-mode.css";
-}
